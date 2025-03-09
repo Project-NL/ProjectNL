2145017818088b2856774cbe1a9e42b9a4350c4f
changgenjor <hoi000115@gmail.com>
2025-03-09 20:15:48 +0900

    fix: 가드 시 기력이 깍이지 않던 오류 수정

--- Source/ProjectNL/Character/Player/PlayerCharacter.cpp
+++ Source/ProjectNL/Character/Player/PlayerCharacter.cpp
@@ -12,6 +12,8 @@
 #include "ProjectNL/GAS/Attribute/PlayerAttributeSet.h"
 #include "ProjectNL/Player/BasePlayerState.h"
 #include "ProjectNL/Helper/EnumHelper.h"
+#include "ProjectNL/Helper/GameplayTagHelper.h"
+#include "ProjectNL/Helper/StateHelper.h"
 #include "ProjectNL/Player/BasePlayerController.h"
 
 APlayerCharacter::APlayerCharacter()
@@ -31,11 +33,11 @@ APlayerCharacter::APlayerCharacter()
 	GetCharacterMovement()->MaxWalkSpeed = 280;
 	GetCharacterMovement()->BrakingDecelerationWalking = 2000.f;
 	GetCharacterMovement()->BrakingDecelerationFalling = 1500.0f;
-	
+
 	//플레이어 스프링 암을 만듭니다
 	PlayerCameraSpringArm = CreateDefaultSubobject<UPlayerSpringArmComponent>(TEXT("PlayerCameraSpringArm"));
 	PlayerCameraSpringArm->SetupAttachment(RootComponent);
-	PlayerCameraSpringArm->TargetArmLength = 400.0f; 
+	PlayerCameraSpringArm->TargetArmLength = 400.0f;
 	PlayerCameraSpringArm->bUsePawnControlRotation = true;
 
 	PlayerCamera = CreateDefaultSubobject<UPlayerCameraComponent>(TEXT("PlayerCamera"));
@@ -44,7 +46,7 @@ APlayerCharacter::APlayerCharacter()
 	PlayerCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
 
 
-	EquipInventoryComponent =CreateDefaultSubobject<UEquipInventoryComponent>(TEXT("EquipInventoryComponent"));
+	EquipInventoryComponent = CreateDefaultSubobject<UEquipInventoryComponent>(TEXT("EquipInventoryComponent"));
 	SetEntityType(EEntityCategory::Player);
 }
 
@@ -63,15 +65,15 @@ void APlayerCharacter::OnRep_PlayerState()
 
 		PlayerAttributeSet = PS->AttributeSet;
 		PlayerAttributeSet->InitBaseAttribute();
-		
+
 		AbilitySystemComponent->InitializeAbilitySystem(InitializeData);
-		
+
 		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
 			PlayerAttributeSet->GetMovementSpeedAttribute()).AddUObject(
 			this, &ThisClass::MovementSpeedChanged);
-		
+
 		AbilitySystemComponent->OnDamageReactNotified
-		.AddDynamic(this, &APlayerCharacter::OnDamaged);
+		                      .AddDynamic(this, &APlayerCharacter::OnDamaged);
 
 		EquipInventoryComponent->InitializeData();
 		Initialize();
@@ -93,13 +95,13 @@ void APlayerCharacter::PossessedBy(AController* NewController)
 		PlayerAttributeSet->InitBaseAttribute();
 
 		AbilitySystemComponent->InitializeAbilitySystem(InitializeData);
-		
+
 		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
 			PlayerAttributeSet->GetMovementSpeedAttribute()).AddUObject(
 			this, &ThisClass::MovementSpeedChanged);
-		
+
 		AbilitySystemComponent->OnDamageReactNotified
-		.AddDynamic(this, &ThisClass::APlayerCharacter::OnDamaged);
+		                      .AddDynamic(this, &ThisClass::APlayerCharacter::OnDamaged);
 
 		EquipInventoryComponent->InitializeData();
 
@@ -110,7 +112,7 @@ void APlayerCharacter::PossessedBy(AController* NewController)
 void APlayerCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 	if (const APlayerController* PC = Cast<APlayerController>(
 		GetController()))
 	{
@@ -127,14 +129,14 @@ void APlayerCharacter::SetupPlayerInputComponent(
 	UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-	
+
 	if (UEnhancedInputComponent* EnhancedInputComponent = Cast<
 		UEnhancedInputComponent>(PlayerInputComponent))
 	{
 		EnhancedInputComponent->BindAction(MoveInputAction, ETriggerEvent::Triggered
-																			, this, &ThisClass::MoveTo);
+		                                   , this, &ThisClass::MoveTo);
 		EnhancedInputComponent->BindAction(LookInputAction, ETriggerEvent::Triggered
-																			, this, &ThisClass::Look);
+		                                   , this, &ThisClass::Look);
 	}
 }
 
@@ -158,7 +160,7 @@ void APlayerCharacter::MoveTo(const FInputActionValue& Value)
 		// TODO: 둘다 동시에 누를 경우 1,1이 되기 때문에 대각선으로 더 멀리갈 수도 있다. (확인 필요)
 		// 그렇기에 값을 조정해줘야할 필요가 있을 수도 있다.
 		const FVector MoveToVector = (ForwardDirection + RightDirection).GetSafeNormal(1);
-		
+
 		this->AddMovementInput(MoveToVector);
 	}
 }
@@ -182,12 +184,23 @@ void APlayerCharacter::OnDamaged(const FDamagedResponse& DamagedResponse)
 {
 	if (PlayerAttributeSet)
 	{
-		// 체력 감소 처리
-		PlayerAttributeSet->SetHealth(PlayerAttributeSet->GetHealth() - DamagedResponse.Damage);
-	}
+		// 플레이어의 경우 스테미나가 30 이상 있어야만 Block 스킬이 발동.
+		if (AbilitySystemComponent->HasMatchingGameplayTag(NlGameplayTags::Status_Guard))
+			{
+				if (PlayerAttributeSet->GetStamina() > 30)
+				{
+					PlayerAttributeSet->SetStamina(PlayerAttributeSet->GetStamina() - 30);
+				}
+			}
+			else
+			{
+				// 체력 감소 처리
+				PlayerAttributeSet->SetHealth(PlayerAttributeSet->GetHealth() - DamagedResponse.Damage);
+			}
+		}
 	FGameplayTagContainer ActiveTags;
 	AbilitySystemComponent->GetOwnedGameplayTags(ActiveTags);
-	DamageResponse=DamagedResponse;
+	DamageResponse = DamagedResponse;
 	// 태그(Status.Sprint, Status.Dodge, Status.Roll) 활성화 여부 체크
 	if (ActiveTags.HasTag(FGameplayTag::RequestGameplayTag(FName("Status.UnderAttack"))))
 	{
@@ -196,36 +209,35 @@ void APlayerCharacter::OnDamaged(const FDamagedResponse& DamagedResponse)
 	if (HasAuthority())
 	{
 		UE_LOG(LogTemp, Log, TEXT("[Server] ReceiveDamage 호출됨: Damage=%f, IsHitStop=%s, SourceActor=%s"),
-			DamagedResponse.Damage,
-			DamagedResponse.IsHitStop ? TEXT("true") : TEXT("false"),
-			*GetNameSafe(DamagedResponse.SourceActor));
+		       DamagedResponse.Damage,
+		       DamagedResponse.IsHitStop ? TEXT("true") : TEXT("false"),
+		       *GetNameSafe(DamagedResponse.SourceActor));
 	}
 	else
 	{
 		UE_LOG(LogTemp, Log, TEXT("[Client] ReceiveDamage 호출됨: Damage=%f, IsHitStop=%s, SourceActor=%s"),
-			DamagedResponse.Damage,
-			DamagedResponse.IsHitStop ? TEXT("true") : TEXT("false"),
-			*GetNameSafe(DamagedResponse.SourceActor));
+		       DamagedResponse.Damage,
+		       DamagedResponse.IsHitStop ? TEXT("true") : TEXT("false"),
+		       *GetNameSafe(DamagedResponse.SourceActor));
 	}
 	// 플레이할 애니메이션 몽타주
 	DamagedMontage = EquipComponent->GetDamagedAnim()
-		.GetAnimationByDirection(DamagedResponse.DamagedDirection, DamagedResponse.DamagedHeight);
+	                               .GetAnimationByDirection(DamagedResponse.DamagedDirection,
+	                                                        DamagedResponse.DamagedHeight);
 	if (DamagedMontage)
 	{
 		float MontageLength = DamagedMontage->GetPlayLength();
-		OnKnockback(DamagedResponse,MontageLength);
+		OnKnockback(DamagedResponse, MontageLength);
 	}
 }
-
 
 
 // Montage가 끝나면 호출되는 함수
 void APlayerCharacter::OnDamagedMontageEnded(UAnimMontage* Montage, bool bInterrupted)
 {
-	
 	// 다음에 중복으로 호출되지 않도록 델리게이트 해제(선택사항)
 
-	
+
 	UAnimInstance* AnimInstance = GetMesh() ? GetMesh()->GetAnimInstance() : nullptr;
 	if (AnimInstance)
 	{
@@ -235,29 +247,30 @@ void APlayerCharacter::OnDamagedMontageEnded(UAnimMontage* Montage, bool bInterr
 
 void APlayerCharacter::OnKnockback(const FDamagedResponse& DamagedResponse, float DamageMontageLength)
 {
-    // 서버에서만 실행 (클라이언트에서 실행 시 바로 리턴)
-    if (!HasAuthority())
-    {
-        UE_LOG(LogTemp, Warning, TEXT("OnKnockback: 클라이언트에서는 실행되지 않습니다."));
-        return;
-    }
-    
-    UE_LOG(LogTemp, Log, TEXT("[Server] OnKnockback 호출됨: Damage=%f, IsHitStop=%s, SourceActor=%s"),
-        DamagedResponse.Damage,
-        DamagedResponse.IsHitStop ? TEXT("true") : TEXT("false"),
-        *GetNameSafe(DamagedResponse.SourceActor));
-    
-    // 1. AbilitySpec 생성 및 AbilitySystemComponent에 부여
-    FGameplayAbilitySpec AbilitySpec = AbilitySystemComponent->BuildAbilitySpecFromClass(DamagedResponse.KnockbackAbility, 1, INDEX_NONE);
-    FGameplayAbilitySpecHandle AbilityHandle = AbilitySystemComponent->GiveAbility(AbilitySpec);
+	// 서버에서만 실행 (클라이언트에서 실행 시 바로 리턴)
+	if (!HasAuthority())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("OnKnockback: 클라이언트에서는 실행되지 않습니다."));
+		return;
+	}
 
-    // 2. AbilityHandle을 통해 AbilitySpec 포인터를 가져옵니다.
-    FGameplayAbilitySpec* Spec = AbilitySystemComponent->FindAbilitySpecFromHandle(AbilityHandle);
-    if (!Spec)
-    {
-        UE_LOG(LogTemp, Warning, TEXT("AbilityHandle에 해당하는 AbilitySpec을 찾을 수 없습니다."));
-        return;
-    }
+	UE_LOG(LogTemp, Log, TEXT("[Server] OnKnockback 호출됨: Damage=%f, IsHitStop=%s, SourceActor=%s"),
+	       DamagedResponse.Damage,
+	       DamagedResponse.IsHitStop ? TEXT("true") : TEXT("false"),
+	       *GetNameSafe(DamagedResponse.SourceActor));
+
+	// 1. AbilitySpec 생성 및 AbilitySystemComponent에 부여
+	FGameplayAbilitySpec AbilitySpec = AbilitySystemComponent->BuildAbilitySpecFromClass(
+		DamagedResponse.KnockbackAbility, 1, INDEX_NONE);
+	FGameplayAbilitySpecHandle AbilityHandle = AbilitySystemComponent->GiveAbility(AbilitySpec);
+
+	// 2. AbilityHandle을 통해 AbilitySpec 포인터를 가져옵니다.
+	FGameplayAbilitySpec* Spec = AbilitySystemComponent->FindAbilitySpecFromHandle(AbilityHandle);
+	if (!Spec)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AbilityHandle에 해당하는 AbilitySpec을 찾을 수 없습니다."));
+		return;
+	}
 	else
 	{
 		UE_LOG(LogTemp, Warning, TEXT("AbilityHandle에 해당하는 AbilitySpec을 찾을 수 있습니다."));
@@ -265,8 +278,8 @@ void APlayerCharacter::OnKnockback(const FDamagedResponse& DamagedResponse, floa
 	}
 	// 3. Spec에서 Primary Instance를 가져옵니다.
 	UGameplayAbility* AbilityInstance = Spec->GetPrimaryInstance();
-    
-	
+
+
 	// 4. UGA_Knockback 타입으로 캐스팅합니다.
 	UGA_Knockback* ActivatedKnockbackAbility = Cast<UGA_Knockback>(AbilityInstance);
 	if (!ActivatedKnockbackAbility)
@@ -284,7 +297,7 @@ void APlayerCharacter::OnKnockback(const FDamagedResponse& DamagedResponse, floa
 	ActivatedKnockbackAbility->SetDamageMontageLength(DamageMontageLength);
 	ActivatedKnockbackAbility->SetDamageMontage(DamagedMontage);
 	//ActivatedKnockbackAbility->OnPlayMontageWithEventDelegate.Clear();
-	
+
 	// 6. Knockback Ability 활성화 시도
 	bool bActivated = AbilitySystemComponent->TryActivateAbilityByClass(DamagedResponse.KnockbackAbility);
 	if (!bActivated)
@@ -292,12 +305,9 @@ void APlayerCharacter::OnKnockback(const FDamagedResponse& DamagedResponse, floa
 		UE_LOG(LogTemp, Warning, TEXT("Knockback Ability 활성화에 실패했습니다."));
 		return;
 	}
-   
 }
+
 UEquipInventoryComponent* APlayerCharacter::GetEquipInventoryComponent()
 {
 	return EquipInventoryComponent;
 }
-
-
-
